f9d762a2abaa796b0a3f34f83f17abd578622e1d
Mansoo Cho <70994968+cmansoo@users.noreply.github.com>
2021-09-26 18:22:22 -0500

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,3 +1,27 @@
 # iterative factorial
+def iter_factorial(n):
+    fact = 1
+    if n in [0,1]:
+        return 1
+    elif n > 1:
+        while n > 0:
+            fact = fact * n
+            n = n - 1
+        return fact
 
-def factorial(n):
+  
+# recursive factorial
+def recur_factorial(n):
+    # base case
+    if n in [0,1]:
+        return 1
+    # recursive case
+    elif n > 1:
+        return n * recur_factorial(n-1)
+    # invalid
+    else:
+        return None
+
+      
+print(iter_factorial(5))
+print(recur_factorial(5))
