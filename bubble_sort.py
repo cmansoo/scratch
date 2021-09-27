1e359ca964e27d6b799c53a23b59d188e5864c90
Mansoo Cho <70994968+cmansoo@users.noreply.github.com>
2021-09-26 22:05:25 -0500

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,23 @@
+import random
+
+# bubble sort
+
+alist = []
+for i in range(10):
+    alist.append(random.randint(1,10))
+
+print(f'unsorted: {alist}')
+
+
+def bubble_sort(alist):
+    size = len(alist)
+    for i in range(size-1):
+        for j in range(0, size-1):
+            if alist[j] > alist[j+1]:
+                temp = alist[j]
+                alist[j] = alist[j+1]
+                alist[j+1] = temp
+    return alist
+
+
+print(f'sorted: {bubble_sort(alist)}')
