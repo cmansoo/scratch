34bae4caeab899ee2e0be8aa0c5db9b9d7905d24
Mansoo Cho <70994968+cmansoo@users.noreply.github.com>
2021-11-18 15:36:36 -0600

    Update search_insert_pos.py

--- search_insert_pos.py
+++ search_insert_pos.py
@@ -12,4 +12,20 @@ def searchInsert(nums: List[int], target: int) -> int:
 # time complexity O(log n)
 # using binary search
 def searchInsert2(nums, target):
-  pass
+    left_ind = 0
+    right_ind = len(nums) - 1
+
+    while left_ind <= right_ind:
+        mid_ind = (left_ind + right_ind) // 2
+        mid_val = nums[mid_ind]
+
+        if mid_val == target:
+            return mid_ind
+
+        elif mid_val < target:
+            left_ind = mid_ind + 1
+
+        else:
+            right_ind = mid_ind - 1
+
+    return right_ind + 1
