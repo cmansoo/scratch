0233edec9d79cdb6c272833060c5c0cb6d66fab4
Mansoo Cho <70994968+cmansoo@users.noreply.github.com>
2024-09-06 22:46:05 -0400

    Create regex_expander.R

---
+++ r script/regex_expander.R
@@ -0,0 +1,56 @@
+#' Regular Expression Bracket Expander
+#'
+#' Given a regular expression with brackets, expands the expression with explicit matches.
+#' Returns a vector of explicit matches.
+#'
+#' @param rex a regular expression
+#' @param show_expanded if set to TRUE, the resulting vector will show each value in the expanded range as names.
+#' @examples
+#' r <- "^W3812|^405[0-3L-O]|^N17[04][9FK]Z"
+#' regex_expander(r)
+regex_expander <- function(rex, show_expanded=TRUE){
+  use_names <- if(!show_expanded) FALSE else TRUE
+  alpha_nums <- c(0:9, letters, LETTERS)
+  rex_split <- strsplit(rex, split="\\|")[[1]]
+  
+  # extract range patterns
+  range_pattern <- stringr::str_extract_all(rex_split, "\\[.*?\\]")
+  
+  # expand range
+  expanded_patterns <- lapply(range_pattern, function(rng){
+    if(length(rng) == 1){
+      grep(rng, alpha_nums, value=TRUE)
+    } else if(length(rng) > 1){
+      # if more than 1 range, get every possible combination
+      expanded <- lapply(rng, function(v) grep(v, alpha_nums, value=TRUE))
+      apply(expand.grid(expanded), 1, function(x) paste0(x, collapse=""))
+    } else{
+      # no range in the pattern
+      NULL
+    }
+  })
+  
+  # replace ranges with explicit nums/alphabets
+  res <- mapply(function(rex, rng, expt){
+    if(length(rng) == 1){
+      sapply(expt, function(p) gsub(pattern=rng, replacement=p, x=rex, fixed=TRUE),
+             USE.NAMES=use_names)
+    } else if(length(rng) > 1){
+      rng <- paste0(rng, collapse="")
+      sapply(expt, function(p) gsub(pattern=rng, replacement=p, x=rex, fixed=TRUE),
+             USE.NAMES=use_names)
+    } else{
+      rex
+    }
+  },
+  rex_split, range_pattern, expanded_patterns,
+  USE.NAMES=FALSE)
+  
+  # return
+  unlist(res)
+}
+
+r <- "^W3812|^405[0-3L-O]|^N17[04][9FK]Z"
+regex_expander(r, FALSE)
+
+
